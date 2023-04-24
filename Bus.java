8f88d88a3e33ed15c2b869da2128315b953f511b
matheustatangelo <mg-tatangelo@hotmail.com.br>
2023-04-24 17:08:14 -0300

    section four of java course 33 video string

---
+++ Bus.java
@@ -0,0 +1,22 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passengers = 0;
+
+        //atribuiu a variável passageiros na variável passageiros e adiciona 9 passageiros
+        passengers = passengers + 9;
+
+        //imprime o valor da variável passageiros
+        System.out.println(passengers);
+
+        //atribuiu a variável passageiros na variável passageiros e subtrai 5 passageiros
+        passengers = passengers - 5;
+
+        //imprime o valor da variável passageiros
+        System.out.println(passengers);
+
+        passengers = passengers - 4;
+        System.out.println(passengers);
+
+
+    }
+}
