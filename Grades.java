5e6ae58d1ba4ea0401d6697ccace3e28ce428df6
matheustatangelo <mg-tatangelo@hotmail.com.br>
2023-04-24 17:36:02 -0300

    section two of 34 char in branch

---
+++ Grades.java
@@ -0,0 +1,12 @@
+public class Grades {
+    public static void main(String[] args) {
+        String potions = "A";
+        String arithmancy = "B";
+        String charms = "A";
+
+        System.out.println("Potions: " + potions);
+        System.out.println("Arithmancy: " + arithmancy);
+        System.out.println("Charms: " + charms);
+
+    }
+}
